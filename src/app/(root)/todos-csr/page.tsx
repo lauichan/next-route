842189a2927bd6e015b168d80132de2fe6e91b2f
lauichan <te06230623@gmail.com>
2024-03-13 19:08:27 +0900

    feat: 통계페이지 바로가기 버튼

--- src/app/(root)/todos-csr/page.tsx
+++ src/app/(root)/todos-csr/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+import TodoForm from "@/components/todos/TodoForm";
+import TodoList from "@/components/todos/TodoList";
+import { useTodosQuery } from "@/lib/todos/query";
+import { Todo } from "@/types/todos";
+import { useRouter } from "next/navigation";
+
 function TodosCSRPage() {
+  const { data = [], isLoading } = useTodosQuery();
+
+  const workingList: Todo[] = data.filter((todo) => !todo.isDone);
+  const doneList: Todo[] = data.filter((todo) => todo.isDone);
+
+  const router = useRouter();
+  const goToReportPage = () => {
+    router.push("/report");
+  };
+
+  if (isLoading) return <div>로딩중...</div>;
+
   return (
     <>
-      <div>TodosCSRPage</div>
+      <button onClick={goToReportPage}>할일정보통계보러가기</button>
+      <TodoForm />
+      <TodoList list={workingList} />
+      <TodoList list={doneList} />
     </>
   );
 }
