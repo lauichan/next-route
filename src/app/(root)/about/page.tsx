0429d0b5ca4e1bae98c4b7105b36a87981c4c702
lauichan <te06230623@gmail.com>
2024-03-13 16:27:06 +0900

    feat: About 페이지 내용 추가

--- src/app/(root)/about/page.tsx
+++ src/app/(root)/about/page.tsx
@@ -1,7 +1,15 @@
-function AboutPage() {
+import { CompanyInfo } from "@/types/companyInfo";
+
+async function AboutPage() {
+  const response = await fetch("http://localhost:3000/api/company");
+  const { data }: { data: CompanyInfo } = await response.json();
+
   return (
     <>
       <div>AboutPage</div>
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <p>{data.image}</p>
     </>
   );
 }
