0631a6993ce0a40989c862d96e9a653ba464de6e
lauichan <te06230623@gmail.com>
2024-03-13 15:31:00 +0900

    feat: @tanstack/react-query 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import QueryProvider from "./provider";
+import Nav from "@/components/nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Nav />
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
