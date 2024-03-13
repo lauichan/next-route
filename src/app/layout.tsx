d4fd178f2667c69302e00e4096f326a18cd104c8
lauichan <te06230623@gmail.com>
2024-03-13 19:05:57 +0900

    rename: 컴포넌트 파일 이름 변경, 가져오기 업데이트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import QueryProvider from "./provider";
-import Nav from "@/components/nav";
+import Nav from "@/components/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
