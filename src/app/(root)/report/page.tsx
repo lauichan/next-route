9d265254db83ad1fc8d65d5c2c3e1fec0c510847
lauichan <te06230623@gmail.com>
2024-03-13 19:07:33 +0900

    feat: 컴포넌트 분리

--- src/app/(root)/report/page.tsx
+++ src/app/(root)/report/page.tsx
@@ -1,39 +1,7 @@
-import { Todo } from "@/types/todos";
+import ReportTable from "@/components/ReportTable";
 
 async function ReportPage() {
-  const response = await fetch("http://localhost:3000/api/todos", {
-    next: {
-      revalidate: 5,
-    },
-  });
-  const { data: todos }: { data: Todo[] } = await response.json();
-
-  const totalCount = todos.length;
-  const workingCount = todos.filter((todo) => todo.isDone === false).length;
-  const doneCount = totalCount - workingCount;
-
-  return (
-    <>
-      <table>
-        <thead>
-          <tr>
-            <th>할일</th>
-            <th>미완료</th>
-            <th>완료</th>
-            <th>전체</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>합계</td>
-            <td>{workingCount}</td>
-            <td>{doneCount}</td>
-            <td>{totalCount}</td>
-          </tr>
-        </tbody>
-      </table>
-    </>
-  );
+  return <ReportTable />;
 }
 
 export default ReportPage;
