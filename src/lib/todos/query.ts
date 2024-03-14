9ff40b34085f28dfe61061f8096dbd11705c001a
lauichan <te06230623@gmail.com>
2024-03-14 21:24:07 +0900

    feat: endpoint 상수로 관리

--- src/lib/todos/query.ts
+++ src/lib/todos/query.ts
@@ -3,11 +3,13 @@ import { Todo } from "@/types/todos";
 import { useQuery } from "@tanstack/react-query";
 import { QUERY_KEYS } from "./query.keys";
 
+export const API_ENDPOINT = "http://localhost:3000/api/todos";
+
 export const useTodosQuery = () => {
   return useQuery({
     queryKey: [QUERY_KEYS],
     queryFn: async () => {
-      const response = await fetch("http://localhost:3000/api/todos");
+      const response = await fetch(`${API_ENDPOINT}`);
       const { data }: { data: Todo[] } = await response.json();
       return data;
     },
